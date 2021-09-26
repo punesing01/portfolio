a1bb1f07d606d3324b4f5f8346fbf5f5c14918fa
punesing01 <puneeshsingh001@gmail.com>
2021-09-25 22:52:23 -0700

    more updates to desktop and mobile view

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ var menu = document.querySelector(".hamburger_menu");
 var modal = document.querySelector(".modal");
 var closeButton = document.querySelector(".close");
 var modalLinks = document.querySelectorAll(".modal_contents__link");
-//var modalLinkedIn = document.querySelector('');
+var introPara = document.querySelector("#myIntroduction");
+var aboutMeOption = document.querySelector('.modal_contents > a:nth-of-type(2)');
 
 menu.addEventListener('click',function(){
     modal.style.display = 'block';
@@ -12,5 +13,11 @@ menu.addEventListener('click',function(){
 });
 
 closeButton.addEventListener('click',function(){
+    modal.style.display = 'none';
+});
+
+
+aboutMeOption.addEventListener('click',function(){
+    //introPara.style.boxShadow= '10px 5px 5px rgb(231 207 73)';
     modal.style.display = 'none';
 });
